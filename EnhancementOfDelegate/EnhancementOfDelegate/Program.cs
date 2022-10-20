9ad19f2674c9ffdee86dd2e2b28551ba458be6dd
Amey Bawane <60310813+ameybawane@users.noreply.github.com>
2022-10-20 22:12:57 +0530

    Add files via upload

---
+++ EnhancementOfDelegate/EnhancementOfDelegate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/*
+ * Mahesh has suggested that we should define one function called 
+ * PerformArithmeticOperation( ) which will take 2 numbers and the 
+ * Delegate as parameters. Instead of calling / executing the 
+ * delegate from Main() function, let this function manage the 
+ * delegate call. You have to implement this functionality.
+ * 
+ * Hint: The PerformArithmeticOperation() function signature is:
+ * 
+ * static void PerformArithmeticOperation( int num1, int num2, MyDelegate arOperation){…}
+ */
+namespace EnhancementOfDelegate
+{
+    internal class Program
+    {
+        delegate int MyDelegate(int num1, int num2);
+        static void Main(string[] args)
+        {
+            MyDelegate arOperation = (int num1, int num2) => num1 + num2;
+            PerformArithmeticOperation(5, 9, arOperation);
+
+            Console.ReadKey();
+        }
+        static void PerformArithmeticOperation(int num1, int num2, MyDelegate arOperation)
+        {
+            var result = arOperation(num1, num2);
+            Console.WriteLine(result);
+        }
+    }
+}
